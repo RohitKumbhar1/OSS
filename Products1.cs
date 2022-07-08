122b8d73257d1868bcc9fdb6a6e014c82c4e426d
RohitKumbhar1 <108925454+RohitKumbhar1@users.noreply.github.com>
2022-07-08 15:39:57 +0530

    .cs file added
    Please check at your side

---
+++ Products1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TFLWebApp.Models
+{
+    //POCO class
+    //Entity Class
+    //Data Annotations defined metadata for class or field, or property
+    [Table("Products1")]
+
+    public class Products1
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [Required]
+        public string TITLE { get; set; }
+        public string DESCRIPTION { get; set; }
+
+        [Range(minimum:50,maximum:2000)]
+        public int QUANTITY { get; set; }
+
+        [Required]
+        public int PRICE { get; set; }
+
+    }
+}
