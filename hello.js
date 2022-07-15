452988194e44d4b8c7ab32dae215c3d8fb15e733
RohitKumbhar1 <108925454+RohitKumbhar1@users.noreply.github.com>
2022-07-15 07:44:53 +0530

    express js
    express

--- hello.js
+++ hello.js
@@ -1,16 +1,3 @@
-<!DOCTYPE html>
 
-<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
-<head>
-    <meta charset="utf-8" />
-    <title></title>
-    <script>
-        document.write("<u> Hello from Java Script </u>");
-    </script>
-</head>
-<body>
-    <script>
-        document.write("<br/> <mark>Hello from Java Script (in body)</mark>");
-    </script>
-</body>
-</html>
+
+console.log("Hello from NodeJS")
